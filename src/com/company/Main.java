81638c42d0f303e8da1af979eba9cb4b9d71b11f
“ADELA <adelutza_2@yahoo.com>
2021-03-29 20:59:59 +0300

    commit 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Heloo Adela");
+        System.out.println("Heloo spring");
+
+
+
 
     }
 }
