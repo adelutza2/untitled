0f2cfc4142085f8f14e7b63cb3ccdfd1b1280a0a
“ADELA <adelutza_2@yahoo.com>
2021-03-29 21:12:22 +0300

    commit 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,12 @@ public class Main {
 	// write your code here
         System.out.println("Heloo Adela");
         System.out.println("Heloo spring");
+        System.out.println(-5 +8*6);
+        System.out.println(3+2);
+        System.out.println(5+9);
+        System.out.println(5/3);
+        System.out.println(7/2);
+        System.out.println((55+9) % 9);
 
 
 
