d1c3617d902fc931c2811bb30c1b74642e4d8542
“ADELA <adelutza_2@yahoo.com>
2021-04-04 17:00:07 +0300

    tema o parte:)

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -1,29 +1,93 @@
 package lab2;
 
+
 public class Main {
     public static void main(String[] args) {
-int i = mult (5,6);
-        System.out.println(i);
-        int ii = imp (100,5);
-        System.out.println(ii);
-        int iii = rez (6,9);
-        System.out.println(iii);
+        printMyName();
+        System.out.println("Rezultatul adunarii este: " + adunare(2, 3));
+
+        System.out.println("Rezultatul scaderii este: " + scadere(17, 5));
+        System.out.println("Rezultatul inmultirii este:" + inmultire(18, 17));
+        System.out.println("Rezultatul impartirii este:" + impartire(120, 60));
+        System.out.println("      j       a        v     v       a");
+        System.out.println("      j      a a        v   v       a a ");
+        System.out.println(" j    j     aaaaa        v v       aaaaa ");
+        System.out.println("  jjj     a       a       v       a      a ");
+        int suma = sum();
+        System.out.println(suma);
+        int inmult = inmult();
+        System.out.println(inmult);
+        System.out.println("  +\"\"\"\"\"+");
+        System.out.println(" [| o o |]");
+        System.out.println("  |  ^  |");
+        System.out.println("  | '-' |");
+        System.out.println("  +-----+");
+        System.out.println(div());
+        double x = 5;
+        x /= 3;
+        System.out.println(x);
+
+
+
     }
 
-    public static int mult(int first, int second) {
-        int mult = first * second ;
-        return mult;
+    {
     }
 
-    public static int imp(int first,int second){
-        int imp = second / first;
-        return imp ;
+    public static int adunare(int primulNumar, int alDoileaNumar) {
+
+        int rezultat = primulNumar + alDoileaNumar;
+
+        return rezultat;
 
     }
-    public static int rez( int first,int second){
-        int rez = second % first ;
-        return rez;
+
+    public static void printMyName() {
+        System.out.println("Heloo \n Adela");
+    }
+
+    public static int scadere(int primulNumar, int alDoileaNumar) {
+
+        int rezultat = primulNumar - alDoileaNumar;
+
+        return rezultat;
+
+    }
+
+    public static int inmultire(int primulNumar, int alDoileaNumar) {
+
+        int rezultat = primulNumar * alDoileaNumar;
+
+        return rezultat;
+    }
+
+    public static int impartire(int primulNumar, int alDoileaNumar) {
+
+        int rezultat = primulNumar / alDoileaNumar;
+
+        return rezultat;
     }
 
 
-}
+    public static int sum() {
+        int sum = 7 + 8 + 6;
+        return sum;
+    }
+
+    public static int inmult() {
+        int inmult = 9 * 9 * 10;
+        return inmult;
+    }
+
+    public static void printFace() {
+        System.out.println();
+    }
+    public static int div(){
+        int div = 185 / 56;
+        return div;
+    }
+
+     }
+
+
+
