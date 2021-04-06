281e7a1d5dd833b28296f6aef843841a54fd2cf0
“ADELA <adelutza_2@yahoo.com>
2021-04-06 09:55:20 +0300

    Animal Rescue

---
+++ src/lab2/AnimalRescue/Food.java
@@ -0,0 +1,23 @@
+package lab2.AnimalRescue;
+
+public class Food {
+    String name = "Berry";
+    String Food = "One";
+    int weight = 20;
+    String colorhair = "Brawn";
+
+    public void callthedog() {
+        System.out.println("Daisy your food for today are berry");
+    }
+
+    public void putthefood() {
+        System.out.println("Here are your berry");
+    }
+
+    public void howmuch() {
+        System.out.println("for today you can eat only 20gr");
+    }
+
+
+}
+
---
+++ src/lab2/AnimalRescue/Vet.java
@@ -0,0 +1,34 @@
+package lab2.AnimalRescue;
+
+public class Vet {
+    String namevet = "Gabriel";
+    String Breed = "Vet";
+    int ani = 36;
+    String colorhair = "Brawn";
+    float weight = 96.2f;
+    int heightincm = 176;
+
+    public void callthedog() {
+        System.out.println("Heloo Daisy,wellcome to the vet!");
+    }
+
+    public void putonthetabel() {
+        System.out.println("Please Aldena ,put the dog on the tabel!");
+    }
+
+    public void control() {
+        System.out.println("Let see how is Daisy today!");
+    }
+
+    public void makecontro() {
+        System.out.println("Hmmm,you are just fine,Daisy!");
+
+    }
+    public void jump(){
+        System.out.println("Jump from the tabel,too see how yoy walk Daisy");
+    }
+    public void seeyou(){
+        System.out.println("See you next month Daisy,if everything is ok!");
+    }}
+
+
